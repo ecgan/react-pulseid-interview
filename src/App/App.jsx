7f0cddabc4bb27a82f2373af7e4e48fd4d007170
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-12-14 16:05:42 +0800

    display app with layout and header bar.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Layout } from 'antd'
+import styles from './App.module.css'
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div
+      className={styles.app}
+    >
+      <Layout>
+        <Layout.Header>
+          <h1>
+            React Image Gallery
+          </h1>
+        </Layout.Header>
+        <Layout.Content>
+          <span>hello world</span>
+        </Layout.Content>
+      </Layout>
     </div>
   )
 }
