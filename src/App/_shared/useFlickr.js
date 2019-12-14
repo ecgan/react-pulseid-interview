5a8984d6d076ab38b5ed3a2fbbcb7e32d3846ec4
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-12-14 22:44:05 +0800

    simplify useFlickr with request abort, query and getRecent.

--- src/App/_shared/useFlickr.js
+++ src/App/_shared/useFlickr.js
@@ -2,7 +2,6 @@ import { useReducer, useEffect } from 'react'
 import flickr from './flickr'
 
 const initialState = {
-  started: false,
   loading: true,
   error: null,
   data: null
@@ -10,64 +9,60 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'start':
-      return {
-        started: true,
-        loading: true,
-        error: null,
-        data: null
-      }
+    case 'fire':
+      return initialState
     case 'error':
       return {
-        started: true,
         loading: false,
         error: action.error,
         data: null
       }
     case 'success':
       return {
-        started: true,
         loading: false,
         error: null,
         data: action.data
-      }
-    case 'abort':
-      return {
-        started: true,
-        loading: false,
-        error: null,
-        data: null
       }
     default:
       return state
   }
 }
 
-const useFlickr = () => {
+const useFlickr = (query) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    if (state.started === false) {
-      dispatch({ type: 'start' })
-      const req = flickr.photos.search({
-        text: 'water',
+    dispatch({
+      type: 'fire'
+    })
+
+    const req = query.text && query.text.trim()
+      ? flickr.photos.search({
+        text: query.text,
+        per_page: 30,
+        page: 1
+      })
+      : flickr.photos.getRecent({
         per_page: 30,
         page: 1
       })
 
-      req.then((res) => {
-        dispatch({
-          type: 'success',
-          data: res.body
-        })
-      }).catch((err) => {
-        dispatch({
-          type: 'error',
-          error: err
-        })
+    req.then((res) => {
+      dispatch({
+        type: 'success',
+        data: res.body
       })
+    }).catch((err) => {
+      dispatch({
+        type: 'error',
+        error: err
+      })
+    })
+
+    return () => {
+      req.abort()
     }
-  })
+  }, [query])
 
   return state
 }
