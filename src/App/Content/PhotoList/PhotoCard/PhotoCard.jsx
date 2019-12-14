60e0350acbc9e7f9cced8dab2f503bc9ce46ccf4
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-12-14 19:52:31 +0800

    styling PhotoCard.

--- src/App/Content/PhotoList/PhotoCard/PhotoCard.jsx
+++ src/App/Content/PhotoList/PhotoCard/PhotoCard.jsx
@@ -2,19 +2,20 @@ import { Card } from 'antd'
 import PropTypes from 'prop-types'
 import React from 'react'
 import PhotoAvatar from './PhotoAvatar/PhotoAvatar'
+import styles from './PhotoCard.module.css'
 
 const PhotoCard = (props) => {
   const { photo } = props
 
   return (
     <Card
+      className={styles.photoCard}
       hoverable
       cover={
         <PhotoAvatar
           photo={photo}
         />
       }
-      style={{ width: 75, height: 75 }}
     />
   )
 }
