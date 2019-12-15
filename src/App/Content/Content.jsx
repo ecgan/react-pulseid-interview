4e8cbc2cda2b34d887bc9fe15c6099f532dac1ea
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-12-16 01:26:43 +0800

    code refactor with separate form UI component.

--- src/App/Content/Content.jsx
+++ src/App/Content/Content.jsx
@@ -1,97 +1,22 @@
-import { Col, DatePicker, Form, Input, Row } from 'antd'
+import { Form } from 'antd'
 import PropTypes from 'prop-types'
 import React from 'react'
-import DebounceInput from '../_shared/DebounceInput/DebounceInput'
 import PhotoSearch from './PhotoSearch/PhotoSearch'
-
-const disableStartDate = (endDate) => (current) => {
-  if (!endDate || !current) {
-    return false
-  }
-
-  return current > endDate
-}
-
-const disableEndDate = (startDate) => (current) => {
-  if (!startDate || !current) {
-    return false
-  }
-
-  return current < startDate
-}
+import SearchForm from './SearchForm/SearchForm'
 
 const Content = (props) => {
   const { form } = props
   const { getFieldDecorator, getFieldValue, setFieldsValue } = form
 
+  getFieldDecorator('page', {
+    initialValue: 1
+  })
+
   return (
     <div>
-      <Form
-        layout='vertical'
-      >
-        {
-          getFieldDecorator('page', {
-            initialValue: 1
-          })(
-            <Input
-              type='hidden'
-            />
-          )
-        }
-        <Form.Item
-          label='Search'
-        >
-          {
-            getFieldDecorator('text')(
-              <DebounceInput />
-            )
-          }
-        </Form.Item>
-        <Form.Item
-          label='Date Taken'
-        >
-          <Row
-            gutter={16}
-          >
-            <Col
-              xs={12}
-            >
-              <Form.Item
-                style={{ marginBottom: 0 }}
-              >
-                {
-                  getFieldDecorator('min_taken_date')(
-                    <DatePicker
-                      disabledDate={disableStartDate(getFieldValue('max_taken_date'))}
-                      format='YYYY-MM-DD'
-                      placeholder='From'
-                      style={{ width: '100%' }}
-                    />
-                  )
-                }
-              </Form.Item>
-            </Col>
-            <Col
-              xs={12}
-            >
-              <Form.Item
-                style={{ marginBottom: 0 }}
-              >
-                {
-                  getFieldDecorator('max_taken_date')(
-                    <DatePicker
-                      disabledDate={disableEndDate(getFieldValue('min_taken_date'))}
-                      format='YYYY-MM-DD'
-                      placeholder='To'
-                      style={{ width: '100%' }}
-                    />
-                  )
-                }
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form.Item>
-      </Form>
+      <SearchForm
+        form={form}
+      />
       <PhotoSearch
         query={form.getFieldsValue()}
         onLoadMore={() => {
